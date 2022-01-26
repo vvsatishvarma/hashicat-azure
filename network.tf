5d9356fec22623b8a8727723485006fd8b7e2122
vvsatishvarma <vvsatishvarma@gmail.com>
2022-01-26 15:15:58 -0600

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,15 +1,15 @@
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "West Europe"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "${var.prefix}-workshop"
+#   location = var.location
+# }
 
 module "network" {
   source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "${var.prefix}-workshop"
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
